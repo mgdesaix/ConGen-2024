9af515cc93a0fbbbbd2bfd2fbf744b4c9b786866
mgdesaix <mgdesaix@gmail.com>
2024-08-26 11:47:25 -0600

    sped up beagle subset script

--- data/scripts/subset-beagle-ind.sh
+++ data/scripts/subset-beagle-ind.sh
@@ -40,21 +40,22 @@ fi
 zcat -f ${beagle} | awk '
 BEGIN { FS=OFS="\t"; headers[1]="marker"; headers[2]="allele1"; headers[3]="allele2"}
 FNR==NR { headers[NR+3]=$1; next }
-        { sep=""
+FNR==1   { sep=""
         for (j=1; j<=length(headers); j++ ){
 	    for (i=1; i<=NF; i++) {
-		if (FNR==1 && ($i == headers[j])) {
+		if ($i == headers[j]) {
 		    k++
 		    cols[k] = i
 		}
 
-	    }
-	    }
-	    for (n=1; n<=length(cols); n++) {
-		    printf "%s%s",sep,$cols[n]
-		    sep=OFS
-	    }
-	    print ""
+	 }
 	}
+} 
+{for (n=1; n<=length(cols); n++) {
+		  printf "%s%s",sep,$cols[n]
+		  sep=OFS
+  }
+  print ""
+}
  ' ${individuals} - | gzip > ${outname}
 
